5e6ba2ba2469689092eab557ab73df37fd8406e8
Piyush <piyushaggarwal20903@gmail.com>
2023-04-04 10:09:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from "./components/HeaderComponent"
+import FooterComponent from "./components/FooterComponent"
+import AddEmployee from './components/AddEmployee';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+      <HeaderComponent/>
+      <div className='container'>
+        <Routes>
+          <Route exact path="/" Component={ListEmployeeComponent}/>
+          <Route path="/employees" Component={ListEmployeeComponent}/>
+          <Route path="/add-employee" Component={AddEmployee}/>
+          <Route path="/edit-employee/:id" Component={AddEmployee}/>
+        </Routes>
+      </div>
+      <FooterComponent/>
+      </Router>
     </div>
   );
 }
---
+++ src/components/AddEmployee.js
@@ -0,0 +1,94 @@
+import React,{useState,useEffect} from 'react'
+import {useNavigate,Link,useParams} from 'react-router-dom';
+import EmployeeService from '../services/EmployeeService'
+
+const AddEmployee = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [emailId, setEmailId] = useState('')
+    const history = useNavigate();
+    const {id} = useParams();
+
+    const saveOrUpdateEmployee=(e)=>{
+        e.preventDefault();
+        const employee = {firstName,lastName,emailId}
+        if(id){
+            EmployeeService.updateEmployee(id,employee).then((response)=>{
+                history('/employees')
+            }).catch(error=>{
+                console.log(error);
+            })
+        }else{
+            EmployeeService.createEmployee(employee).then((response)=>{
+                console.log(response.data)
+    
+                history('/employees');
+            }).catch(error=>{
+                console.log(error)
+            })
+        }   
+    }
+    useEffect(() => {
+      EmployeeService.getEmployeeById(id).then((response)=>{
+        setFirstName(response.data.firstName)
+        setLastName(response.data.lastName)
+        setEmailId(response.data.emailId)
+      }).catch(error=>{
+        console.log(error)
+      })
+    }, [])
+    
+    const title = ()=>{
+        if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+        }else{
+            <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
+  return (
+    <div>
+        <br /><br /><br />
+        <div className='container'>
+            <div className='row'>
+                <div className='card col-md-6 offset-md-3 offset-md-3'>
+                    {
+                        title()
+                    }
+                    <div className='card-body'>
+                        <form>
+                            <div className='form-group mb-2'>
+                                <label className='form-label'>
+                                    First Name:
+                                </label>
+                                <input type="text" placeholder='Enter first name' name='firstName' className='form-control'
+                                value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+                            </div>
+                            <div className='form-group mb-2'>
+                                <label className='form-label'>
+                                    Last Name:
+                                </label>
+                                <input type="text" placeholder='Enter Last name' name='lastName' className='form-control'
+                                value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+                            </div>
+                            <div className='form-group mb-2'>
+                                <label className='form-label'>
+                                   Email ID:
+                                </label>
+                                <input type="email" placeholder='Enter email ID' name='emailId' className='form-control'
+                                value={emailId} onChange={(e)=>setEmailId(e.target.value)}/>
+                            </div>
+
+                            <button className='btn btn-success' onClick={(e)=>saveOrUpdateEmployee(e)}>Submit</button>
+                            <Link to='/employees' className='btn btn-danger'>Cancel </Link>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddEmployee
